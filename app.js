97af6b9b743653006f01b711d7d1f61b0abcbf1b
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2024-04-23 18:19:05 +0100

    able to move 7 angles according to the xAxis

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const card = document.querySelector(".card");
 const productImage = document.querySelector(".productImage");
 
 //movement mouse sensitive
-container.addEventListener("mousemove", (e) => {
-  let xAxis = window.innerWidth / 2 - e.pageX;
-  console.log(xAxis);
-  if (xAxis > 100) {
-    productImage.src = `./figure_Left.PNG`;
-  } else if (xAxis < -100) {
-    productImage.src = `./figure_Right.PNG`;
-  } else if (xAxis >= -100 || xAxis <= 100) {
-    productImage.src = `./figure_front.PNG`;
+card.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / -1;
+  let imageIndex;
+
+  //starting from the left to right
+  if (xAxis < -800) {
+    imageIndex = `Left03`;
+  } else if (xAxis < -600) {
+    imageIndex = `Left02`;
+  } else if (xAxis < -400) {
+    imageIndex = `Left01`;
+  } else if (xAxis >= -400 && xAxis <= 400) {
+    imageIndex = `front`;
+  } else if (xAxis <= 800) {
+    imageIndex = `Right01`;
+  } else if (xAxis <= 1000) {
+    imageIndex = `Right02`;
+  } else {
+    imageIndex = `Right03`;
   }
+
+  productImage.src = `./figure_${imageIndex}.PNG`;
+});
+// bounce back to front when mouse leaves card
+card.addEventListener("mouseleave", (e) => {
+  productImage.src = `./figure_front.PNG`;
 });
